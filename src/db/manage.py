f0144a1d4fd629c787139ccc0beca548862d5a9d
C6STR <c00kie_1dsprst@yahoo.co.jp>
2019-05-17 20:02:00 +0900

    migrateファイル作成できた!

--- src/db/manage.py
+++
@@ -1,2 +0,0 @@
-if __name__ == '__main__':
-    main(debug='False', url='mysql+pymysql://root:passl@localhost:3306/test?charset=utf8', repository='.')
---
+++ src/db/migrate/versions/001_add_test_task_table.py
@@ -0,0 +1,37 @@
+# -*- encoding:utf-8 -*-
+from sqlalchemy import *
+from migrate import *
+import enum
+from sqlalchemy import (Column, String, Text, ForeignKey, \
+                create_engine, MetaData, DECIMAL, DATETIME, exc, event, Index)
+from sqlalchemy.schema import UniqueConstraint
+from sqlalchemy.orm import (sessionmaker, relationship, scoped_session)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import INTEGER as Integer
+from datetime import datetime
+
+meta = MetaData()
+
+table = Table(
+    'books', meta,
+    Column('task-id', Integer, primary_key=True),
+    Column('task-name', String(255), nullable=False),
+    Column('types',Integer, nullable=False),
+    Column('priority',Integer , nullable=False),
+    Column('status',Integer , nullable=False),
+    Column('start-daytime' , DATETIME , nullable=False),
+    Column('end-daytime' , DATETIME , nullable=False),
+)
+    
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    meta.bind = migrate_engine
+    table.create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    meta.bind = migrate_engine
+    table.drop()
