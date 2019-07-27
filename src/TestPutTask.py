2c6e7bacf6bd407d7e31cbc8b0c08187a0b76dd3
C6STR <c00kie_1dsprst@yahoo.co.jp>
2019-07-27 22:14:31 +0900

    テストがうまくいかないので同じディレクトリに移動

--- src/TestPutTask.py
+++ src/TestPutTask.py
@@ -4,10 +4,10 @@ import json
 pt = PutTask.PutTask()
 
 data = {
-    "task-name" : "インサートするタスク" ,
+    "user_id" : "user_id_001",
+    "task-name" : "タスク名" ,
     "types" : 1,
     "priority" : 1,
-    "status" : 1,
     "start-daytime" : "2019-1-1",
     "end-daytime": "2019-1-2"
 }
