68db0e96c1631d352ee19499948716cb22c033c3
C6STR <c00kie_1dsprst@yahoo.co.jp>
2019-07-28 11:00:28 +0900

    GetTasklit 本体

--- src/GetTaskLists.py
+++ src/GetTaskLists.py
@@ -1,17 +1,24 @@
+import Db
+import json
+
 class GetTaskList():
     def __init__(self):
-        pass
+        self.db = Db.connect()
     
-    def get_task_list(self):
-        #これはよくない
-        json_sample = {
-            "task-id":1,
-            "task-name":"たすくてすと",
-            "type":"memo",
-            "priority":"high",
-            "status":"Doing",
-            "start-daytime":"2019-01-12",
-            "start-daytime":"2019-01-15",
-            "child-task-id":2
-        }
-        return json_sample
+    def get_task_list(self , json_data):
+        cur = self.db.cursor()
+        data = json.loads(json_data)
+
+        get_task_list_sql = """
+        select task_name , type , priority , status , start_daytime , end_daytime 
+        from task 
+        where user_id = '%s'
+        """
+
+        cur.execute(get_task_list_sql %(
+            data["uuid"]
+        ))
+
+        rows = cur.fetchall()
+
+        return rows
